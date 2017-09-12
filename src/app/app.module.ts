9781eae227fe2450be528dc9d4137660ce697d09
carotene512 <karoten512@gmail.com>
2017-09-12 18:31:48 +0900

    セオ用に作り変え

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,22 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MdSidenavModule,
+  MdCardModule,
+} from '@angular/material';
+import {
+  AgmCoreModule,
+  GoogleMapsAPIWrapper
+} from '@agm/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  NgModule,
+  ApplicationRef
+} from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +25,20 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDfnrYNQtMC-PqTWvH9F36TehlAN6Zw3MM'
+    }),
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdSidenavModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
